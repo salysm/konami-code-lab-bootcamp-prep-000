a9dc44fca5427e2306a94eb3ade443317ccad325
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-11-26 22:28:30 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -14,36 +14,18 @@ const codes = [
 function init() {
   // your code here
 }
-const keyCodes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
+ var index = 0;
+  document.addEventListener('keydown', function (e) {
+    const key = parseInt(e.detail || e.which);
 
-let index = 0;
+    if (key === code[index]) {
+      index++;
 
-document.body.addEventListener("keydown", (thingPressed) => {
-  const key = thingPressed.key 
-  console.log(key) 
-  
-  if (keyCodes[index] == key) {
-    ++index 
-  } 
-  
-  else {
-    idx = 0 
-  }
-  
-  console.log(index)
-  if (index == 10) {
-    alert("Hurray!");
-    index = 0 
-  }
-});
+      if (index === code.length - 1) {
+        window.alert("YOU DID IT!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }, false)
