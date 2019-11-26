1df638345a72c358fa0439b506857b1a57f226c2
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-11-26 21:59:56 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -14,7 +14,7 @@ const codes = [
 function init() {
   // your code here
 }
-
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
  function init() { var index = 0;
   document.addEventListener('keydown', function (e) {
     const key = parseInt(e.detail || e.which);
